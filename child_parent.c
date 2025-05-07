1b5f1bf5b6e0a4c6f509e09a7e8f5857df32be88
CH PranayKumar <23r01a0577@gmail.com>
2025-05-07 23:10:31 +0530

    Add files via upload

---
+++ child_parent.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("Fork failed");
+        return 1;
+    } else if (pid == 0) {
+        printf("child\n");
+    } else {
+        printf("parent\n");
+    }
+
+    return 0;
+}
